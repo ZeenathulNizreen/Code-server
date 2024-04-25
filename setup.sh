36977a4c3128e35542eb899f36df21bc23ddaf93
Zeenathul Nizreen <34499535+ZeenathulNizreen@users.noreply.github.com>
2024-04-25 15:30:44 +0530

    Update setup.sh
    updating setup script file with session management tmux

--- setup.sh
+++ setup.sh
@@ -14,5 +14,6 @@ wget https://raw.githubusercontent.com/ZeenathulNizreen/Code-server/main/ngrok.p
 # Set up ngrok with auth token
 python ngrok.py --ngrok_auth_token $2
 
-# Tunnel given port to ngrok server
-ngrok http $1 & /usr/bin/code-server --port $1 & sleep 5s && cat /root/.config/code-server/config.yaml
+# Tunnel given port to ngrok server with tmux session managemnet
+tmux new -d -s mysession "/usr/bin/code-server --port $1 & ngrok http $1 & sleep 5s && cat /root/.config/code-server/config.yaml"
+
