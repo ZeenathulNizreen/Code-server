8c4d97945d5e9ff7e71ea1db2fe5e1a7162d3c8d
Zeenathul Nizreen <34499535+ZeenathulNizreen@users.noreply.github.com>
2024-04-25 16:34:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,19 +5,14 @@ wget https://raw.githubusercontent.com/coder/code-server/main/install.sh
 chmod +x install.sh
 ./install.sh
 
-# Install pyngrok
+# Install pyngrok and fire
 pip install pyngrok fire
 
-# Download ngrok.py from this gist
+# Download ngrok.py from the specified repository
 wget https://raw.githubusercontent.com/ZeenathulNizreen/Code-server/main/ngrok.py
 
-# Set up ngrok with auth token
-python ngrok.py --ngrok_auth_token $2
+# Set up ngrok with auth token provided as the second argument
+python ngrok.py --ngrok_auth_token "$2"
 
-# Tunnel given port to ngrok server with tmux session managemnet
-tmux new -d -s mysession "source /root/FedAgg/.venv/bin/activate && /usr/bin/code-server --port $1 & ngrok http $1 & sleep 5s && cat /root/.config/code-server/config.yaml"
-
-# Tunnel given port to ngrok server
-# ngrok http $1 & /usr/bin/code-server --port $1 & sleep 5s && cat /root/.config/code-server/config.yaml
-
-
+# Tunnel the given port to ngrok server and manage it within a tmux session
+tmux new -d -s mysession "source /root/FedAgg/.venv/bin/activate && ngrok http $1 > /root/ngrok.log 2>&1 & sleep 10s && /usr/bin/code-server --port $1 > /root/code-server.log 2>&1 & sleep 5s && cat /root/.config/code-server/config.yaml"
